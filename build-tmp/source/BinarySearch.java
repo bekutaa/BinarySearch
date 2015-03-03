9cf4bf60eb19c61468d409b511bd4f7996625743
bekutaa <erika.auyeung@sbcglobal.net>
2015-03-03 10:06:12 -0800

    Assignment completed.

--- build-tmp/source/BinarySearch.java
+++ build-tmp/source/BinarySearch.java
@@ -1,3 +1,19 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class BinarySearch extends PApplet {
+
 
 private Item[] store = {
         new Item(184,14),
@@ -57,7 +73,7 @@ public int binarySearch(int catNumToFind,int nLow, int nHigh)
             return store[guess].getInventory();
         else if(store[guess].getCatNum() < catNumToFind)
             return binarySearch(catNumToFind,guess+1,nHigh);
-        else
+        else //if(store[guess].getCatNum() > catNumToFind)
             return binarySearch(catNumToFind,nLow,guess-1);
     }
     else
@@ -111,3 +127,12 @@ public void draw()
 
 
 
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "BinarySearch" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
